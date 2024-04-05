276ec1690e154f0610fea0d25bc762db0f73a611
Nelson <nelsonjnunez@gmail.com>
2024-04-04 22:16:46 -0300

    no message

--- TestVocacional/Extensions/PreguntaExtension.cs
+++ TestVocacional/Extensions/PreguntaExtension.cs
@@ -5,7 +5,7 @@ namespace TestVocacional.Extensions
 {
     public static class PreguntaExtension
     {
-        public static List<string> VerificarIntereses(this List<Pregunta> preguntas, List<Interes> listaintereses)
+        public static List<string> VerificarRespuestas(this List<Pregunta> preguntas, List<Interes> listaintereses)
         {
             return preguntas
                 .Select(pregunta => pregunta.ID.VerificarDiccionario(listaintereses))
@@ -17,18 +17,6 @@ namespace TestVocacional.Extensions
                 .ToList();
         }
         
-        public static List<string> VerificarAptitudes(this List<Pregunta> preguntas, List<Interes> listaintereses)
-        {
-            return preguntas
-                .Select(pregunta => pregunta.ID.VerificarDiccionario(listaintereses))
-                .Where(letra => letra != null)
-                .GroupBy(letra => letra)
-                .OrderByDescending(grupo => grupo.Count())
-                .Take(2)
-                .Select(grupo => grupo.Key)
-                .ToList();
-        }
-
         public static string VerificarDiccionario(this int id, List<Interes> listaintereses)
         {
             foreach(var inters in listaintereses)
